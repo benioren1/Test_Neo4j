44f4b08ef96325c91b29bd328758f5431b6f17f4
benioren1 <benioren123beni@gmail.com>
2024-11-28 11:07:06 +0200

    i started the project and i created bku prints to the load data and create tracteer.

--- blu_printes/load_data_routes.py
+++ blu_printes/load_data_routes.py
@@ -5,6 +5,7 @@ from flask import Blueprint, request
 bp_load = Blueprint("load",__name__)
 
 
+
 @bp_load.route("/api/phone_tracker", methods=["POST"])
 def create_phone_tracker():
     # TODO: Implement phone tracker loading
